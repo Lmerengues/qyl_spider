bb65a3522791306ab5335c4bb5ab077cb19541fd
lmerengues <317958662@qq.com>
2018-05-30 22:11:53 +0800

    add make zip

--- qyl/qyl/view.py
+++ qyl/qyl/view.py
@@ -5,10 +5,36 @@ from django.shortcuts import render
 import os
 from django.conf import settings
 from django.http import HttpResponse
+import zipfile
+
+img_path = "/home/obe60/qyl_spider/img/"
 
 def hello(request):
 
-    if os.path.exists("/home/obe60/qyl_spider/img/25249"):
-        return HttpResponse("1")
+    try:
+        id = request.GET["id"]
+    except:
+        id = "25539"
+
+    img_dir = img_path + id
+    if os.path.exists(img_dir):
+        make_zip(img_path,"/home/obe60/"+id+".zip")
+        with open("/home/obe60/"+id+".zip") as f:
+            c = f.read()
+        return HttpResponse(c)
     else:
-        return HttpResponse("0")
+        return HttpResponse("error occurred!")
+
+
+
+def make_zip(source_dir, output_filename):
+    zipf = zipfile.ZipFile(output_filename, 'w')
+    pre_len = len(os.path.dirname(source_dir))
+    for parent, dirnames, filenames in os.walk(source_dir):
+        for filename in filenames:
+            pathfile = os.path.join(parent, filename)
+            arcname = pathfile[pre_len:].strip(os.path.sep)
+            zipf.write(pathfile, arcname)
+    zipf.close()
+
+
